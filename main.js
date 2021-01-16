51e6fa8c5566ba1679c4dc91b8a65344ff4b48cd
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-16 11:07:56 -0600

    added variables, data types and math to js file

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log("CONNECTED");
+// console.log("CONNECTED");
+
+// ********** START VARIABLES *************
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+firstName = 'Dr. T';
+lastName = 'Vasquez';
+
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Gumbo';
+// console.log(favoriteFood);
+
+// ******** END VARIABLES **********
+
+// ******** START DATA TYPES **********
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// ******** END DATA TYPES **********
+
+// ******** START MATH **********
+// console.log(Math.pow(2, 2)); // 4
+// console.log(Math.pow(3, 2)); // 9
+// console.log(Math.pow(3, 3)); // 27
+
+// console.log(Math.round(6.5)); // 7
+// console.log(Math.round(6.45)); // 6
+// console.log(Math.floor(6.9999)); // 6
+// console.log(Math.ceil(6.0000001)); // 7
+
+const cat = 'kitty';
+console.log(cat.length); // 5
+
+// ******** END MATH **********
