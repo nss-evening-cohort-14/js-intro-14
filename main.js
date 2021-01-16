428e12228896f49d1d3b47c888b72786a27eec44
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-01-16 13:09:13 -0600

    worked through conditional logic

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("CONNECTED");
+// console.log("CONNECTED");
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1 !== 'nice' && arg2 !== 'Nice') {
+//     console.log(`You are ${arg1}`);
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1.length < 3) {
+//     console.log(`You are ${arg1}`);
+//   } else if (arg2.length > 8) {
+//     console.log(`You are ${arg2}`);
+//   } else {
+//     console.log('You are NOTHING!');
+//   }
+// }
+
+// thisIsTrue('ni', 'mickeymouse');
+
+const canDrive = (age) => {
+  if (age >= 15) {
+    return true;
+  }
+
+  return false;
+}
+
+const answer = canDrive(12);
+console.log(answer);
